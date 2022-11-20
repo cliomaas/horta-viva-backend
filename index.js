3ae0fcd05c44bca68325f34f77197e73a11898c1
cliomaas <cliomaas@gmail.com>
2022-11-20 17:45:20 -0300

    added logic

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
 const PORT = process.env.PORT || 8000
 const express = require('express')
 const axios = require('axios')
+const cors = require('cors')
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.json('hi')
+app.use(cors())
+
+app.get('/cpf', (req, res) => {
+    const cpf = req.query.cpf;
+
+    if (cpf == '') res.send({ response: 'invalido' });
+    // Elimina CPFs invalidos conhecidos	
+    if (cpf.length != 11 ||
+        cpf == "00000000000" ||
+        cpf == "11111111111" ||
+        cpf == "22222222222" ||
+        cpf == "33333333333" ||
+        cpf == "44444444444" ||
+        cpf == "55555555555" ||
+        cpf == "66666666666" ||
+        cpf == "77777777777" ||
+        cpf == "88888888888" ||
+        cpf == "99999999999")
+        res.send({ response: 'invalido' });
+    // Valida 1o digito	
+    add = 0;
+    for (i = 0; i < 9; i++)
+        add += parseInt(cpf.charAt(i)) * (10 - i);
+    rev = 11 - (add % 11);
+    if (rev == 10 || rev == 11)
+        rev = 0;
+    if (rev != parseInt(cpf.charAt(9)))
+        res.send({ response: 'invalido' });
+    // Valida 2o digito	
+    add = 0;
+    for (i = 0; i < 10; i++)
+        add += parseInt(cpf.charAt(i)) * (11 - i);
+    rev = 11 - (add % 11);
+    if (rev == 10 || rev == 11)
+        rev = 0;
+    if (rev != parseInt(cpf.charAt(10)))
+        res.send({ response: 'invalido' });
+    res.send({ response: 'valido' });
 })
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
